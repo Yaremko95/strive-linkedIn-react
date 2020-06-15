f9d857358f0c1b734c424c91c7abb617323e1532
tetiana <tetianayaremko@gmail.com>
2020-06-15 16:47:20 +0200

    merges footer

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { createUseStyles } from "react-jss";
 import NavBar from "./NavBar";
+import Footer from "../components/Footer";
 
 function MainLayout(props) {
   const useStyles = createUseStyles((theme) => ({
@@ -17,6 +18,7 @@ function MainLayout(props) {
         {" "}
         className={classes.main}>{props.children}
       </Container>
+      <Footer />
     </>
   );
 }
