d1738b2c311f5c2b18a69a4ee9f65f6bbb2450eb
tetiana <tetianayaremko@gmail.com>
2020-06-15 16:14:35 +0200

    merge

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import { createUseStyles } from "react-jss";
+import NavBar from "./NavBar";
 
 function MainLayout(props) {
   const useStyles = createUseStyles((theme) => ({
@@ -9,7 +10,15 @@ function MainLayout(props) {
     },
   }));
   const classes = useStyles();
-  return <Container className={classes.main}>{props.children}</Container>;
+  return (
+    <>
+      <NavBar />
+      <Container>
+        {" "}
+        className={classes.main}>{props.children}
+      </Container>
+    </>
+  );
 }
 
 export default MainLayout;
