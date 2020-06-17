b62bafafc1b719e4ab8ba285d85c184a30b5a560
Hammad-87 <pakswim@gmail.com>
2020-06-17 16:35:31 +0200

    News feed

---
+++ src/components/ui/NewsFeed/NewsFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NFCard from './NFCard'
+
+export default function NewsFeed(props) {
+
+    return (
+        <div>
+            <NFCard {...props}/>
+        </div>
+    )
+}
